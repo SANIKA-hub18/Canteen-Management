25f40b62185c29f37de1bf3b755d9427cfe9259d
Sanika Shaligram <shaligramsanika007@gmail.com>
2025-09-16 11:26:29 +0530

    Update flask_api.py

--- flask_api/flask_api.py
+++ flask_api/flask_api.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "*"}})
 
 # MySQL Config 
-app.config['MYSQL_HOST'] = os.getenv('MYSQL_HOST', 'localhost')
+app.config['MYSQL_HOST'] = os.getenv('MYSQL_HOST', '*')
 app.config['MYSQL_USER'] = os.getenv('MYSQL_USER', '*')
 app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD', '*')
 app.config['MYSQL_DB'] = os.getenv('MYSQL_DB', '*')
